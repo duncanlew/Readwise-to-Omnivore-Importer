d22dfd05db200dd85522b7938d6c06f4c2c10dbb
Duncan Lew <duncanlew@users.noreply.github.com>
2023-11-28 19:52:03 +0100

    some stuff can be read in

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use std::{error::Error, io, process};
 
+#[derive(Debug, serde::Deserialize)]
+struct Record {
+    #[serde(rename(deserialize = "Title"))]
+    title: String,
+    #[serde(rename(deserialize = "URL"))]
+    url: String,
+    #[serde(rename(deserialize = "Document tags"))]
+    document_tags: String,
+    #[serde(rename(deserialize = "Saved date"))]
+    saved_date: String,
+    #[serde(rename(deserialize = "Reading progress"))]
+    reading_progress: String,
+    #[serde(rename(deserialize = "Location"))]
+    location: String,
+    #[serde(rename(deserialize = "Seen"))]
+    seen: String
+}
+
 fn example() -> Result<(), Box<dyn Error>> {
     let mut reader = csv::Reader::from_reader(io::stdin());
-    for result in reader.records() {
-        let record = result?;
+    for result in reader.deserialize() {
+        let record: Record = result?;
         println!("{:?}", record);
     }
     Ok(())
