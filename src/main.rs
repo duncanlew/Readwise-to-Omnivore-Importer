ac855fccf4b64b9d22add9d1f7d32eef48428032
Duncan Lew <duncanlew@users.noreply.github.com>
2023-11-29 21:34:17 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,10 @@ struct Article {
     #[serde(rename(deserialize = "Location"))]
     location: String,
     #[serde(rename(deserialize = "Seen"))]
-    seen: String
+    seen: String,
 }
 
-fn example() -> Result<(), Box<dyn Error>> {
+fn get_imported_articles() -> Result<(Vec<Article>), Box<dyn Error>> {
     let mut csv_reader = csv::Reader::from_path("test.csv")?;
     let (errors, articles): (Vec<_>, Vec<Article>) = csv_reader
         .deserialize()
@@ -33,12 +33,15 @@ fn example() -> Result<(), Box<dyn Error>> {
     println!("{:#?}", articles);
     println!("{:#?}", errors);
 
-
-    Ok(())
+    if errors.is_empty() {
+        Ok(articles)
+    } else {
+        Err("Errors occurred while reading the CSV".into())
+    }
 }
 
 fn main() {
-    if let Err(err) = example() {
+    if let Err(err) = get_imported_articles() {
         println!("Error running example: {}", err);
         process::exit(1);
     }
