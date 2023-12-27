5e4a4c866616575e209f185b2dcc816ad0204bf3
Duncan Lew <duncanlew@users.noreply.github.com>
2023-12-27 22:47:37 +0100

    Cleanup code and save with the date (#4)

--- src/main.rs
+++ src/main.rs
@@ -40,12 +40,13 @@ fn get_imported_articles() -> Result<(Vec<Article>), Box<dyn Error>> {
     }
 }
 
-async fn save_url(article_url: String, is_archived: bool) -> Result<(), Box<dyn Error>> {
+async fn save_url(article_url: String, saved_date: String, is_archived: bool) -> Result<(), Box<dyn Error>> {
     let mut input_map = serde_json::Map::new();
 
     input_map.insert("clientRequestId".to_string(), Value::String(format!("{}", Uuid::new_v4())));
     input_map.insert("source".to_string(), Value::String("api".to_string()));
     input_map.insert("url".to_string(), Value::String(format!("{}", article_url)));
+    input_map.insert("savedAt".to_string(), Value::String(format!("{}", saved_date)));
     input_map.insert("labels".to_string(), json!([{"name": "imported"}]));
     if is_archived {
         input_map.insert("state".to_string(), Value::String("ARCHIVED".to_string()));
@@ -68,43 +69,8 @@ async fn save_url(article_url: String, is_archived: bool) -> Result<(), Box<dyn 
         .header("authorization", "MY API KEY SHOULD BE HERE")
         .send()
         .await?;
-    // "{\"data\":{\"saveUrl\":{\"url\":\"https://omnivore.app/aquapika/links/f1b52de3-fe9e-48a0-8fc7-717b43e38531\",\"clientRequestId\":\"f1b52de3-fe9e-48a0-8fc7-717b43e38531\"}}}\n"
-    let something = result.text().await?;
-    println!("Resulting body {:#?}", something);
-    Ok(())
-}
-
-// TODO this can be removed?
-async fn set_link_archived(article_url: String) -> Result<(), Box<dyn Error>> {
-    let payload = json!({
-        "query": "mutation SetLinkArchived($input: ArchiveLinkInput!) { \
-            setLinkArchived(input: $input) { \
-                ... on ArchiveLinkSuccess { linkId message } \
-                ... on ArchiveLinkError { message errorCodes } \
-                } \
-            }",
-        "variables": {
-            "input": {
-                "linkId": "",
-                "archived": true,
-                "clientRequestId": format!("{}", Uuid::new_v4()),
-                "source": "api",
-                "url": format!("{}", article_url),
-                "labels": [{
-                    "name": "imported"
-                }]
-            }
-        }
-    });
-
-    let client = reqwest::Client::new();
-    client.post("https://api-prod.omnivore.app/api/graphql")
-        .json(&payload)
-        .header("content-type", "application/json")
-        .header("authorization", "MY API KEY SHOULD BE HERE")
-        .send()
-        .await?;
-
+    let result_body = result.text().await?;
+    println!("Resulting body {:#?}", result_body);
     Ok(())
 }
 
@@ -120,7 +86,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // imported_articles.iter().for_each(|article: &Article| println!("{:#?}", article));
 
     let article_url = imported_articles.get(0).unwrap().url.to_string();
-    save_url(article_url, false).await
+    let saved_date = imported_articles.get(0).unwrap().saved_date.to_string();
+    save_url(article_url, saved_date, false).await
         .unwrap_or_else(|error| {
             eprintln!("error occurred")
         });
