adf1689956e63620a16da529f49caac6ef25ebdd
Duncan Lew <duncanlew@users.noreply.github.com>
2023-12-30 20:10:22 +0100

    partitioning the different results

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process::exit;
 
 use clap::Parser;
 
-use crate::structs::Arguments;
+use crate::structs::{Arguments, ImportedArticle};
 
 mod structs;
 mod csv_parser;
@@ -13,16 +13,27 @@ mod omnivore_lib;
 async fn main() -> Result<(), Box<dyn Error>> {
     let arguments = Arguments::parse();
 
-    let imported_articles = csv_parser::get_imported_articles(arguments.file_path)
+    let imported_articles = csv_parser::get_imported_articles(&arguments.file_path)
         .unwrap_or_else(|err| {
             eprintln!("Error occurred: {}\nExiting application", err);
             exit(1);
         });
 
-    let results = omnivore_lib::save_urls(arguments.key, imported_articles).await;
+    let results: Vec<ImportedArticle> = omnivore_lib::save_urls(arguments.key, imported_articles).await;
+    let (success_results, rest_results): (Vec<ImportedArticle>, Vec<ImportedArticle>) =  results.into_iter().partition(|result| result.successful);
+    let (invalid_results, error_results): (Vec<ImportedArticle>, Vec<ImportedArticle>) = rest_results.into_iter().partition(|result| result.is_invalid_url);
+
     // TODO remove these lines
-    println!("\n*************************\nDone with async requests in the main function");
-    println!("{:#?}", results);
+    println!("\n*************************\nInvalid results");
+    println!("{:#?}", invalid_results);
+    println!("*************************\nError results");
+    println!("{:#?}", error_results);
+    println!("*************************\nSuccess results");
+    println!("{:#?}", success_results);
+    println!("\n########################################");
+    println!("Invalid count: {}", invalid_results.len());
+    println!("Error count: {}", error_results.len());
+    println!("Success count: {}", success_results.len());
 
     Ok(())
 }
