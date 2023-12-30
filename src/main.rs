75a962862e15f563b66a569cb4bb8922df9ecabd
Duncan Lew <duncanlew@users.noreply.github.com>
2023-12-30 14:54:36 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,14 @@
 mod structs;
 mod csv_parser;
+mod omnivore_lib;
 
 use std::error::Error;
 use std::process::exit;
-use std::sync::Arc;
 use itertools::Itertools;
 use serde::Deserialize;
-use serde_json::{json, Map, Value};
-use uuid::Uuid;
 use clap::Parser;
-use futures::{stream, StreamExt};
-use reqwest::Client;
-use crate::structs::{Arguments, Article};
-
-async fn save_urls(key: String, imported_articles: Vec<Article>) {
-    let atomic_key = Arc::new(key);
-    let client = Client::new();
-    stream::iter(imported_articles)
-        .for_each_concurrent(None, |article| {
-            let key = Arc::clone(&atomic_key).to_string();
-            let client = client.clone();
-            async move {
-                let article_url = article.url.to_string();
-                let saved_date = article.saved_date.to_string();
-                let location = article.location.to_string();
-                let is_archived = location == "archive";
-                let input = create_input(article_url, saved_date, is_archived);
-
-                save_url(input, key, client)
-                    .await
-                    .unwrap_or_else(|error| {
-                        eprintln!("Error has occurred during the saving of URLs into Omnivore:\n{}", error);
-                    });
-            }
-        })
-        .await;
-}
-
-async fn save_url(input: Map<String, Value>, key: String, client: Client) -> Result<(), Box<dyn Error>> {
-    let payload = json!({
-        "query": "mutation SaveUrl($input: SaveUrlInput!) { \
-            saveUrl(input: $input) { \
-                ... on SaveSuccess { url clientRequestId } \
-                ... on SaveError { errorCodes message } \
-                } \
-            }",
-        "variables": {
-            "input": input
-        }
-    });
-
-    let result = client.post("https://api-prod.omnivore.app/api/graphql")
-        .json(&payload)
-        .header("content-type", "application/json")
-        .header("authorization", key)
-        .send()
-        .await;
-
-    match result {
-        Ok(response) => {
-            if response.status().is_success() {
-                // TODO remove these two lines at the end
-                let result_body = response.text().await?;
-                println!("Resulting body {:#?}", result_body);
-                Ok(())
-            } else {
-                let status = response.status();
-                let text = response.text().await?;
-                let error_message = format!("Server returned the code \"{}\" and the message {}", status, text);
-                Err(error_message.into())
-            }
-        }
-        Err(error) => {
-            let error_message = format!("Error while processing request: {}", error);
-            Err(error_message.into())
-        }
-    }
-}
-
-fn create_input(article_url: String, saved_date: String, is_archived: bool) -> Map<String, Value> {
-    let mut input_map = serde_json::Map::new();
-    input_map.insert("clientRequestId".to_string(), Value::String(format!("{}", Uuid::new_v4())));
-    input_map.insert("source".to_string(), Value::String("api".to_string()));
-    input_map.insert("url".to_string(), Value::String(format!("{}", article_url)));
-    // TODO place this back
-    // input_map.insert("savedAt".to_string(), Value::String(format!("{}", saved_date)));
-    input_map.insert("labels".to_string(), json!([{"name": "imported"}]));
-    if is_archived {
-        input_map.insert("state".to_string(), Value::String("ARCHIVED".to_string()));
-    }
-    input_map
-}
+use futures::StreamExt;
+use crate::structs::Arguments;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -102,7 +20,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             exit(1);
         });
 
-    save_urls(arguments.key, imported_articles).await;
+    omnivore_lib::save_urls(arguments.key, imported_articles).await;
 
     println!("Successfully imported csv into Omnivore");
     Ok(())
---
+++ src/omnivore_lib.rs
@@ -0,0 +1,88 @@
+use std::sync::Arc;
+use reqwest::Client;
+use futures::{stream, StreamExt};
+use serde_json::{json, Map, Value};
+use std::error::Error;
+use uuid::Uuid;
+use crate::structs::Article;
+
+pub async fn save_urls(key: String, imported_articles: Vec<Article>) {
+    let atomic_key = Arc::new(key);
+    let client = Client::new();
+    stream::iter(imported_articles)
+        .for_each_concurrent(None, |article| {
+            let key = Arc::clone(&atomic_key).to_string();
+            let client = client.clone();
+            async move {
+                let article_url = article.url.to_string();
+                let saved_date = article.saved_date.to_string();
+                let location = article.location.to_string();
+                let is_archived = location == "archive";
+                let input = create_input(article_url, saved_date, is_archived);
+
+                save_url(input, key, client)
+                    .await
+                    .unwrap_or_else(|error| {
+                        eprintln!("Error has occurred during the saving of URLs into Omnivore:\n{}", error);
+                    });
+            }
+        })
+        .await;
+}
+
+fn create_input(article_url: String, saved_date: String, is_archived: bool) -> Map<String, Value> {
+    let mut input_map = serde_json::Map::new();
+    input_map.insert("clientRequestId".to_string(), Value::String(format!("{}", Uuid::new_v4())));
+    input_map.insert("source".to_string(), Value::String("api".to_string()));
+    input_map.insert("url".to_string(), Value::String(format!("{}", article_url)));
+    // TODO place this back
+    // input_map.insert("savedAt".to_string(), Value::String(format!("{}", saved_date)));
+    input_map.insert("labels".to_string(), json!([{"name": "imported"}]));
+    if is_archived {
+        input_map.insert("state".to_string(), Value::String("ARCHIVED".to_string()));
+    }
+    input_map
+}
+
+async fn save_url(input: Map<String, Value>, key: String, client: Client) -> Result<(), Box<dyn Error>> {
+    let payload = json!({
+        "query": "mutation SaveUrl($input: SaveUrlInput!) { \
+            saveUrl(input: $input) { \
+                ... on SaveSuccess { url clientRequestId } \
+                ... on SaveError { errorCodes message } \
+                } \
+            }",
+        "variables": {
+            "input": input
+        }
+    });
+
+    let result = client.post("https://api-prod.omnivore.app/api/graphql")
+        .json(&payload)
+        .header("content-type", "application/json")
+        .header("authorization", key)
+        .send()
+        .await;
+
+    match result {
+        Ok(response) => {
+            if response.status().is_success() {
+                // TODO remove these two lines at the end
+                let result_body = response.text().await?;
+                println!("Resulting body {:#?}", result_body);
+                Ok(())
+            } else {
+                let status = response.status();
+                let text = response.text().await?;
+                let error_message = format!("Server returned the code \"{}\" and the message {}", status, text);
+                Err(error_message.into())
+            }
+        }
+        Err(error) => {
+            let error_message = format!("Error while processing request: {}", error);
+            Err(error_message.into())
+        }
+    }
+}
+
+
