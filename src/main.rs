a7a12b354b5e02f6cd387f8c5f8c7f3481a078f9
Duncan <post@duncanlew.nl>
2023-12-31 20:49:20 +0100

    something

--- src/main.rs
+++ src/main.rs
@@ -23,18 +23,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let (success_results, rest_results): (Vec<ImportedArticle>, Vec<ImportedArticle>) =  results.into_iter().partition(|result| result.successful);
     let (invalid_results, error_results): (Vec<ImportedArticle>, Vec<ImportedArticle>) = rest_results.into_iter().partition(|result| result.is_invalid_url);
 
+
     // TODO remove these lines
-    let invalid_count = invalid_results.len();
-    let error_count = error_results.len();
-    let success_count = success_results.len();
-    let total_count = invalid_count + error_count + success_count;
     println!("\n*************************\nInvalid results");
     println!("{:#?}", invalid_results);
     println!("*************************\nError results");
     println!("{:#?}", error_results);
     println!("*************************\nSuccess results");
     println!("{:#?}", success_results);
-    println!("\n########################################");
+    // End of TODO
+    
+    let invalid_count = invalid_results.len();
+    let error_count = error_results.len();
+    let success_count = success_results.len();
+    let total_count = invalid_count + error_count + success_count;
+    println!("\n=============================================");
     println!("Total processed: {}", total_count);
     println!("\tInvalid count: {}", invalid_count);
     println!("\tError count: {}", error_count);
