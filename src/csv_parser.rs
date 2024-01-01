5cce3da2fb14d31e3844ad70e0153c4c7dfa2858
Duncan <post@duncanlew.nl>
2024-01-01 17:26:16 +0100

    this works...

--- src/csv_parser.rs
+++ src/csv_parser.rs
@@ -24,10 +24,14 @@ pub fn get_imported_articles(file_path: &str) -> Result<Vec<Article>, Box<dyn Er
     }
 }
 
-pub fn write_logs(articles: Vec<Article>, invalid_results: Vec<ImportedArticle>, error_results: Vec<ImportedArticle>){
+pub fn write_logs(articles: Vec<Article>, invalid_results: Vec<ImportedArticle>, error_results: Vec<ImportedArticle>) {
     let timestamp = Local::now().format("%Y-%m-%d--%H-%M-%S").to_string();
 
     let invalid_urls: std::collections::HashSet<&str> = invalid_results
+        .iter()
+        .map(|imported_article| imported_article.url.as_str())
+        .collect();
+    let error_urls: std::collections::HashSet<&str> = error_results
         .iter()
         .map(|imported_article| imported_article.url.as_str())
         .collect();
@@ -35,7 +39,11 @@ pub fn write_logs(articles: Vec<Article>, invalid_results: Vec<ImportedArticle>,
     write_logs_for_invalid_articles(&timestamp, invalid_urls, articles)
         .unwrap_or_else(|err| {
             eprintln!("Error occurred during the saving of the logs for invalid articles {}", err);
-        })
+        });
+    // write_logs_for_error_articles(&timestamp, error_urls, &articles)
+    //     .unwrap_or_else(|err| {
+    //         eprintln!("Error occurred during the saving of the logs for error articles {}", err);
+    //     });
 }
 
 fn write_logs_for_invalid_articles(timestamp: &str, invalid_urls: std::collections::HashSet<&str>, articles: Vec<Article>) -> Result<(), Box<dyn Error>> {
@@ -50,4 +58,14 @@ fn write_logs_for_invalid_articles(timestamp: &str, invalid_urls: std::collectio
     Ok(())
 }
 
-fn write_logs_for_error_articles() {}
+fn write_logs_for_error_articles(timestamp: &str, error_urls: std::collections::HashSet<&str>, articles: &Vec<Article>) -> Result<(), Box<dyn Error>> {
+    let mut wtr = csv::Writer::from_path(format!("invalid-articles-{}.csv", timestamp))?;
+
+    articles
+        .iter()
+        .filter(|article| error_urls.contains(article.url.as_str()))
+        .try_for_each(|article| wtr.serialize(article))?;
+
+    wtr.flush()?;
+    Ok(())
+}
